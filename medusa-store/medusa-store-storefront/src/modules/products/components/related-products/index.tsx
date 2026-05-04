ff09731afbad7b388fbc284e3b074d5b7fbefddc
neraj <nerajnerajlal@gmail.com>
2026-05-04 11:43:45 +0530

    images styles

--- medusa-store/medusa-store-storefront/src/modules/products/components/related-products/index.tsx
+++ medusa-store/medusa-store-storefront/src/modules/products/components/related-products/index.tsx
@@ -1,7 +1,7 @@
 import { listProducts } from "@lib/data/products"
 import { getRegion } from "@lib/data/regions"
 import { HttpTypes } from "@medusajs/types"
-import Product from "../product-preview"
+import { ProductCard } from "@modules/raleys/components/ProductCard"
 
 type RelatedProductsProps = {
   product: HttpTypes.StoreProduct
@@ -47,23 +47,10 @@ export default async function RelatedProducts({
   }
 
   return (
-    <div className="product-page-constraint">
-      <div className="flex flex-col items-center text-center mb-16">
-        <span className="text-base-regular text-gray-600 mb-6">
-          Related products
-        </span>
-        <p className="text-2xl-regular text-ui-fg-base max-w-lg">
-          You might also want to check out these products.
-        </p>
-      </div>
-
-      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8">
-        {products.map((product) => (
-          <li key={product.id}>
-            <Product region={region} product={product} />
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-8 gap-y-12">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} region={region} />
+      ))}
     </div>
   )
 }
